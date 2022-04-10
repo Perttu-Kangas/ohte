56e84b2cac47b6f262a0772a51942f0f8e99f355
DeeCaaD <perttu.kangas@hotmail.fi>
2022-04-10 17:27:14 +0300

    Quick refactor to game

--- harjoitustyo/src/index.py
+++ harjoitustyo/src/index.py
@@ -1,11 +1,4 @@
-import pygame
 from services.snake_game import SnakeGame
 
-pygame.init()
-pygame.display.set_caption("Snake Game")
 snake_game = SnakeGame()
-
-while True:
-    if snake_game.tick():
-        pygame.quit()
-        break
+snake_game.start()
--- harjoitustyo/src/services/snake_game.py
+++ harjoitustyo/src/services/snake_game.py
@@ -7,50 +7,66 @@ from entities.apple import Apple
 class SnakeGame:
 
     def __init__(self):
+        self.snake_game_loop = None
         self.game_x = 300
         self.game_y = 300
-        self.display = pygame.display.set_mode((self.game_x, self.game_y))
         self.difficulty = Difficulty.EASY
         self.apple_color = Color.RED
         self.snake_color = Color.BLACK
         self.background_color = Color.GREEN
         self.snake = Snake(self, 150, 150)
         self.apple = Apple(self)
+
+    def start(self):
+        self.snake_game_loop = SnakeGameLoop(self)
+        while True:
+            if self.snake_game_loop.tick():
+                pygame.quit()
+                break
+
+
+class SnakeGameLoop:
+
+    def __init__(self, snake_game: SnakeGame):
+        pygame.init()
+        pygame.display.set_caption("Snake Game")
+        self.game = snake_game
+        self.display = pygame.display.set_mode((self.game.game_x, self.game.game_y))
         self.clock = pygame.time.Clock()
 
     def tick(self):
         if not self.handle_events():
             return True
-        if self.snake.collides():
+        if self.game.snake.collides():
             return True
-        if self.apple.collides(self.snake):
-            self.snake.grow()
-            self.apple.move()
+        if self.game.apple.collides(self.game.snake):
+            self.game.snake.grow()
+            self.game.apple.move()
         self.draw()
         pygame.display.update()
-        self.clock.tick(self.difficulty.value)
+        self.clock.tick(self.game.difficulty.value)
 
     def draw(self):
-        self.display.fill(self.background_color.value)
-        for body_part in self.snake.body:
-            pygame.draw.rect(self.display, self.snake_color.value, pygame.Rect(body_part[0], body_part[1], 10, 10))
-        pygame.draw.rect(self.display, self.apple_color.value, pygame.Rect(self.apple.x, self.apple.y, 10, 10))
+        self.display.fill(self.game.background_color.value)
+        for body_part in self.game.snake.body:
+            pygame.draw.rect(self.display, self.game.snake_color.value, pygame.Rect(body_part[0], body_part[1], 10, 10))
+        pygame.draw.rect(self.display, self.game.apple_color.value, pygame.Rect(self.game.apple.x, self.game.apple.y, 10, 10))
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    self.snake.move(Direction.LEFT)
+                    self.game.snake.move(Direction.LEFT)
                 elif event.key == pygame.K_RIGHT:
-                    self.snake.move(Direction.RIGHT)
+                    self.game.snake.move(Direction.RIGHT)
                 elif event.key == pygame.K_UP:
-                    self.snake.move(Direction.UP)
+                    self.game.snake.move(Direction.UP)
                 elif event.key == pygame.K_DOWN:
-                    self.snake.move(Direction.DOWN)
+                    self.game.snake.move(Direction.DOWN)
                 else:
-                    self.snake.move(None)
+                    self.game.snake.move(None)
                 return True
             elif event.type == pygame.QUIT:
                 return False
-        self.snake.move(None)
+        self.game.snake.move(None)
         return True
