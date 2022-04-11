afb84b73e8994df29abe4b7fa318dfa48982e54a
DeeCaaD <perttu.kangas@hotmail.fi>
2022-04-11 20:03:11 +0300

    Added pygame to pylint whitelist

--- harjoitustyo/src/entities/apple.py
+++ harjoitustyo/src/entities/apple.py
@@ -5,10 +5,10 @@ class Apple:
 
     def __init__(self, snake_game):
         self.snake_game = snake_game
-        self.x = random.randrange(
-            1, (snake_game.game_x // 10)) * 10  # pylint: disable=invalid-name
-        self.y = random.randrange(
-            1, (snake_game.game_y // 10)) * 10  # pylint: disable=invalid-name
+        self.x = random.randrange(  # pylint: disable=invalid-name
+            1, (snake_game.game_x // 10)) * 10
+        self.y = random.randrange(  # pylint: disable=invalid-name
+            1, (snake_game.game_y // 10)) * 10
 
     def move(self):
         self.x = random.randrange(1, (self.snake_game.game_x // 10)) * 10
