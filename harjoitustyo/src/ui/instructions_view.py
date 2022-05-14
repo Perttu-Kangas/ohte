2b5454c3ea026cfafbd223fdcc5670ec68eb16c3
DeeCaaD <perttu.kangas@hotmail.fi>
2022-05-14 16:20:47 +0300

    Added instructions

--- harjoitustyo/src/ui/instructions_view.py
+++ harjoitustyo/src/ui/instructions_view.py
@@ -26,6 +26,13 @@ class InstructionsView:
 
         info_label = ttk.Label(master=self.frame, text="Peliohjeet")
 
+        move_label = ttk.Label(master=self.frame, text="1. Liikuminen tapahtuu nuolinäppäimillä")
+        points_label = ttk.Label(master=self.frame, text="2. Pisteitä saa syömällä omenoita")
+        border_label = ttk.Label(master=self.frame, text="3. Osuessa reunaan tai matoon peli loppuu")
+
         back_button.grid(row=0, column=0, sticky=constants.EW,
                          padx=10, pady=10)
         info_label.grid(row=1, column=0, sticky=constants.EW, padx=10, pady=10)
+        move_label.grid(row=3, column=0, sticky=constants.EW, padx=10, pady=10)
+        points_label.grid(row=4, column=0, sticky=constants.EW, padx=10, pady=10)
+        border_label.grid(row=5, column=0, sticky=constants.EW, padx=10, pady=10)
